8be7415cbc46e659dc882e311fea8caa3fe1d128
tkubanyc <tkubanyc@student.42heilbronn.de>
2024-12-27 22:24:55 +0100

    ex00: chore: Create main function

---
+++ ex00/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkubanyc <tkubanyc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/27 20:48:42 by tkubanyc          #+#    #+#             */
+/*   Updated: 2024/12/27 21:41:46 by tkubanyc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main( void ) {
+
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return 0;
+}
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,13 +6,16 @@
 /*   By: tkubanyc <tkubanyc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 20:41:52 by tkubanyc          #+#    #+#             */
-/*   Updated: 2024/12/27 20:46:29 by tkubanyc         ###   ########.fr       */
+/*   Updated: 2024/12/27 20:51:35 by tkubanyc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
+#include <iostream>
+
+// Swap two values
 template <typename T>
 void	swap( T& a, T& b ) {
 	T temp = a;
@@ -20,11 +23,13 @@ void	swap( T& a, T& b ) {
 	b = temp;
 }
 
+// Return the smaller of two values
 template <typename T>
 const T&	min( const T& a, const T& b ) {
 	return ( a < b ) ? a : b;
 }
 
+// Return the larger of two values
 template <typename T>
 const T&	max( const T& a, const T& b ) {
 	return ( a > b ) ? a : b;
